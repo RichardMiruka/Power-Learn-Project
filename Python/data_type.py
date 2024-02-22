f450b80554fc8a824ad55e77fadba67431ee48d8
RichardMiruka <richardonsare15@gmail.com>
2024-02-22 18:02:57 +0300

    using the type() function to know which class a variable or value belongs to

---
+++ Python/data_type.py
@@ -0,0 +1,9 @@
+num1 = 5
+print(num1, "is of type", type(num1))  # Output: 5 is of type <class 'int'>
+
+num2 = 3.2
+print(num2, "is of type", type(num2))  # Output: 3.2 is of type <class 'float'>
+
+num3 = 1 +2j
+print(num3, "is of type", type(num3))  # Output: (1+2j) is of type <class 'complex'>
+
