61062a9309d952c519a8327a9677e357723c3c00
RichardMiruka <richardonsare15@gmail.com>
2024-02-22 11:55:49 +0300

    declaring and printing variables using print function in dart

---
+++ Dart/variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  //declaring variables
+  String name = "dart class";
+  int age = 76;
+  double price = 100.14;
+  var year = 2024;
+  num students = 460;
+
+  //printing variables using print function
+
+  print(name);
+  print(age);
+  print(price);
+  print(year);
+  print(students);
+}
