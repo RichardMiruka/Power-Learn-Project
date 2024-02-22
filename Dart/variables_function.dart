5ce468833b7618145f68cd1cfb3fdcfd2d204df0
RichardMiruka <richardonsare15@gmail.com>
2024-02-22 13:52:29 +0300

    printing variables using print function

---
+++ Dart/variables_function.dart
@@ -0,0 +1,15 @@
+void main() {
+  //declaring variables
+  String name = "John"; //must mee in quotes
+  String address = "Nairobi, Kenya";
+  num age = 20; //num can be used for both int and double. It is a super type of int and double
+  num height = 5.6; //decimal numbers
+  bool isMarried = false; //boolean value - true or false
+
+  //printing variables using print function
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married status is $isMarried");
+}
