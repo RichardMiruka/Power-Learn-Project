7818facfedc58b4eb30adca22a00e1c91da47094
RichardMiruka <richardonsare15@gmail.com>
2024-02-22 15:18:15 +0300

    Python variables

---
+++ Python/Lesson_variables.py
@@ -0,0 +1,15 @@
+#assign value to site_name variable
+site_name = "Power Learn project"
+
+print(site_name)
+
+#assigning a new value to the site_name variable
+
+site_name = "Power Learn project 2.0"
+print(site_name)
+
+#use the input function to get user's name from keyboard and assign it to the variable user_name
+#user_name = input("Please enter your name: ")
+
+#display a welcome message using f-string that includes the user's name 
+#print(f"\nWelcome {user_name}! Let's learn something new today!")
