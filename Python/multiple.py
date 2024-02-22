3dba1afa4dd5e145feea5323ecce07d422ed3c05
RichardMiruka <richardonsare15@gmail.com>
2024-02-22 15:25:29 +0300

    assigning multiple values to multiple variables

---
+++ Python/multiple.py
@@ -0,0 +1,11 @@
+#assigning multiple values to multiple variables
+
+a, b, c = 5, 3.2, "Hello"
+print(f"{a} {b} {c}")
+# Output: 5 3.2 Hello
+
+#print(a) # Output: 5
+#print(b) # Output: 3.2
+#print(c) # Output: Hello
+
+# Rearranging the order will give an error because not all variables are assigned a value
