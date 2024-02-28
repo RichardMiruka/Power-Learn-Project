e83a2d84db4ebc6aba5c10ae406296e9fceae37f
RichardMiruka <richardonsare15@gmail.com>
2024-02-28 10:41:36 +0300

    notes on Python data types

--- Python/data_type.py
+++ Python/data_type.py
@@ -7,3 +7,19 @@ print(num2, "is of type", type(num2))  # Output: 3.2 is of type <class 'float'>
 num3 = 1 +2j
 print(num3, "is of type", type(num3))  # Output: (1+2j) is of type <class 'complex'>
 
+# data type represents a kind of value and determines 
+# how the value can be used in expressions and operations.
+
+# A variable (container in a computer memory)is a name that refers to a value. 
+# In Python, variables can be assigned different data types.
+# Python has the following data types built-in by default, in these categories:
+
+# Numeric Types: int, float, complex
+# Text Type: str
+# Sequence Types: list, tuple, range
+# Mapping Types: dict
+# Set Types: set, frozenset
+# Boolean Type: bool
+# Binary Types: bytes, bytearray, memoryview
+
+# You can get the data type of any object by using the type() function:
