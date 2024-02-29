3e3cb86d62e80753410d342d16546c587e8b3a92
RichardMiruka <richardonsare15@gmail.com>
2024-02-29 14:03:59 +0300

    explanatory notes on  boolean data types in dart

---
+++ Dart/data_types/boolean_data_types.dart
@@ -0,0 +1,29 @@
+// In Dart, boolean holds either true or false value.
+// You can write the bool keyword to define the boolean data type. 
+//You can use boolean if the answer is true or false.
+
+void main() {
+  //declaring variables
+  bool isMarried = true;
+  bool isSingle = false;
+
+  //printing info
+  print("isMarried is $isMarried");
+  print("isSingle is $isSingle");
+}
+
+/*
+OUTPUT:
+isMarried is true
+isSingle is false
+*/ 
+
+// In the above example, we have declared two boolean variables isMarried and isSingle.
+// The variable "isMarried" has been assigned a value of true and the variable "isSingle" has been assigned a value of false.
+
+// The print statement prints the value of the variables isMarried and isSingle. 
+// Since the variable "isMarried" holds a value of true, the output will be "isMarried is true". 
+// Similarly, for "isSingle", since it holds a value of false, the output will be "isSingle is false". 
+// This is how you can use boolean data types in Dart. 
+
+ 
