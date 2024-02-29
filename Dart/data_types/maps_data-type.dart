39489a1deefb75d4cbf4197e6cc544bce078eaa8
RichardMiruka <richardonsare15@gmail.com>
2024-02-29 20:31:24 +0300

    explanatory notes with examples on Maps data types in dart

---
+++ Dart/data_types/maps_data-type.dart
@@ -0,0 +1,27 @@
+// A map is a dynamic collection that represents 
+// a set of values ​as key-value pairs.
+// Keys and values ​in the map can be of any type 
+// Maps are similar to dictionaries in python or objects in JavaScript.
+
+void main() {
+  //declaring map
+  Map<String, int> marks = {
+    "John": 90,
+    "Doe": 80,
+    "Smith": 70
+  };
+
+  //printing map
+  print("Marks are $marks");
+
+  //accessing elements
+  print("John's marks are ${marks["John"]}");
+  print("Doe's marks are ${marks["Doe"]}");
+  print("Smith's marks are ${marks["Smith"]}");
+
+  //adding elements
+  marks["Alex"] = 85;
+  marks["Alice"] = 95;
+
+  print("Updated marks are $marks");
+}
