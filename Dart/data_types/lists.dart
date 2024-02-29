f9b4bfb2b21e4a87d357c111a2ff837b93285955
RichardMiruka <richardonsare15@gmail.com>
2024-02-29 16:51:14 +0300

    lists data types in dart

--- Dart/data_types/lists.dart
+++ Dart/data_types/lists.dart
@@ -6,7 +6,7 @@
 void main() {
   List<String> names = ["John", "James", "Peter"];
   print("Value of names is $names");
-  print("value of names[0] is ${names[0]}"); //index 0
+  print("Value of names[0] is ${names[0]}"); //index 0
   print("Value of names[1] is ${names[1]}"); //index 1
   print("Value of names[2] is ${names[2]}"); //index 2
 
