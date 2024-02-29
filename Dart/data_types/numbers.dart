4a74a4f1f13b898dc52c999541572d927277d44b
RichardMiruka <richardonsare15@gmail.com>
2024-02-29 13:13:35 +0300

    numbers data type in dart

---
+++ Dart/data_types/numbers.dart
@@ -0,0 +1,17 @@
+void main() {
+//declaring variables
+  int num1 = 100;
+  double num2 = 130.2;
+  num num3 = 50;
+  num num4 = 50.4;
+
+  //For Summing up the values of all four numbers, we can use the following code:
+  num sum = num1 + num2 + num3 + num4;
+
+//printing Info
+  print("Num 1 is $num1");
+  print("Num 2 is $num2");
+  print("Num 3 is ${num3}");
+  print("Sum of Num 1 to Num 4 is $sum");
+}
+
