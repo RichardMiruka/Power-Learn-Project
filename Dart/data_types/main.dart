23b3b0c90b20c29cd970e883dc28b8be65d704c4
RichardMiruka <richardonsare15@gmail.com>
2024-02-29 11:18:24 +0300

    first dart program on data types

---
+++ Dart/data_types/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  //declaring a variable
+  int num1 = 100;
+  double num2 = 3.14;
+  num num3 = 150; // num can store both int and double
+  num num4 = 73.4;
+
+  num sum = num1 + num2 + num3 + num4;
+
+  print("Sum of $num1, $num2, $num3 and $num4 is $sum");
+}
