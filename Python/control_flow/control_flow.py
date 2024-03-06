543e95dd0e4ee56aba180e4e442e942bf427be5f
RichardMiruka <richardonsare15@gmail.com>
2024-03-06 10:44:56 +0300

    first control flow statement

---
+++ Python/control_flow/control_flow.py
@@ -0,0 +1,10 @@
+# Control Flow
+marks = 80
+result = ""
+if marks <30:
+    result = "Fail"
+elif marks > 75:
+    result = "Passed with distinction"
+else:
+    result = "Passed"
+print(result)        
