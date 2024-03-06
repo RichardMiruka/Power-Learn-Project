bd63d956b059a33e2d7dc5cf1565230f74a3bf91
RichardMiruka <richardonsare15@gmail.com>
2024-03-06 11:17:33 +0300

    match statements in control flow

---
+++ Python/control_flow/bike.py
@@ -0,0 +1,9 @@
+bike = 'cadilla'
+
+if bike == 'Hero':
+    print('This is a Hero bike')
+elif bike in ['Cadillac', 'Harley Davidson']:
+    print('This is a Cadillac or Harley Davidson bike')
+else:
+    print('This is not a Hero bike')    
+# Output: This is not a Hero bike
