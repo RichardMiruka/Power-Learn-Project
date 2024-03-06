12de58d2d3d0472ad1c6d55d77f137dd7f9366dc
RichardMiruka <richardonsare15@gmail.com>
2024-03-06 11:41:24 +0300

    continue control statement

---
+++ Python/control_flow/break_jump_statement.py
@@ -0,0 +1,9 @@
+x = 0
+while x < 10:
+    print("x:", x)
+    if x == 5:
+        print("Breaking...")
+        break
+    x += 1
+print("End")
+      
---
+++ Python/control_flow/continue_jump_statement.py
@@ -0,0 +1,4 @@
+for letter in "Python":
+    if letter == "h":
+        continue
+    print("Current letter:", letter) # Output: P, y, t, o, n
