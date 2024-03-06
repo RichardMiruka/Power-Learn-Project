612d81a168dd07183fcb60ab6dde3f7f0ed3d857
RichardMiruka <richardonsare15@gmail.com>
2024-03-06 12:41:16 +0300

    continue control statement

--- Python/control_flow/continue_jump_statement.py
+++ Python/control_flow/continue_jump_statement.py
@@ -1,4 +1,4 @@
-for letter in "Python":
+for letter in "Python": # continue when letter is not "h"
     if letter == "h":
         continue
     print("Current letter:", letter) # Output: P, y, t, o, n
