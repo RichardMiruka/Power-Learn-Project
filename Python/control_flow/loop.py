05409fdd4ff39a3342b092af79cab0a34781bf06
RichardMiruka <richardonsare15@gmail.com>
2024-03-06 11:32:10 +0300

    while loop function

---
+++ Python/control_flow/loop.py
@@ -0,0 +1,7 @@
+words = ["one", "two", "three", "four", "five", "six"]
+for x in words:
+    print(x)
+    
+# the for loops iterates over the list of words and prints each word in the list. 
+# The loop continues until all the elements in the list are printed. 
+# In this case, it will print one, two, three, four, five, and six.
---
+++ Python/control_flow/while_loop.py
@@ -0,0 +1,5 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+print("Loop is finished.")
