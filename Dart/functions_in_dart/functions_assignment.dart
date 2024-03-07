35d8d80d732993e4b0dc9d069ecdba4630219927
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 15:56:44 +0300

    function to add two numbers

---
+++ Dart/functions_in_dart/functions_assignment.dart
@@ -0,0 +1,11 @@
+// Task 1
+// Function to add two numbers
+int addTwo(int a, int b) {
+  int sum = a + b;
+  return sum;
+}
+
+void main() {
+  // calling the functions with parameters and printing the result
+  print('The sum is ${addTwo(30, 20)}'); // output: The sum is 50
+}
