e98d007234383801b6d29bad5ed12305b9b26efa
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 10:30:40 +0300

    if else control statement'

---
+++ Dart/control_flow/else_if.dart
@@ -0,0 +1,11 @@
+void main() {
+  var age = 18;
+  if (age > 18) {
+    print('Mariam is a voter in Kenya');
+  } else if (age == 18) {
+    print('Mariam just became eligible to vote in Kenya');
+
+  } else {
+    print('Mariam is still young to vote');
+  }
+}
