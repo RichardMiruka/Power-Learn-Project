60a98f8a9eaafb4b74cda358a7c7f18f18a9b4ff
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 14:09:27 +0300

    an anonymous function to print all list items

---
+++ Dart/functions_in_dart/anonymus_function.dart
@@ -0,0 +1,6 @@
+void main() { // main function
+  const fruits = ['apples', 'oranges', 'mangoes', 'bananas']; // list of fruits
+  fruits.forEach((fruit) { // anonymous function, arrow function can be used as well
+    print(fruit); // prints each fruit in the list one by one
+  });
+}
