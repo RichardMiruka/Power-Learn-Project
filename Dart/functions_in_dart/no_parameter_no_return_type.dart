bfba5017b42a09712805ad367dd9f5c7f97fe4de
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 13:57:06 +0300

    Function with no parameter & Return type

---
+++ Dart/functions_in_dart/no_parameter_no_return_type.dart
@@ -0,0 +1,13 @@
+void main() {
+  //Function with no parameter & Return type
+  String name = InstructorsName();
+  print("The name from function is $name");
+}
+String InstructorsName(){
+  return "Richard Miruka";
+}
+// Functions can also have parameters.
+// The parameters are the values that are passed to the function when it is called.
+// In the above example, the function InstructorsName() has no parameters.
+// The parameters are specified within the pair of parenthesis. 
+
