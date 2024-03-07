fbf6bb3ed524592f52cf7d0f93f617d3e0e9ec0b
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 12:43:19 +0300

    function that prints name

---
+++ Dart/functions_in_dart/main.dart
@@ -0,0 +1,10 @@
+// writing function outside main function.
+void printName(){
+  print('My name is Richard Miruka');
+}
+
+// this is our main function.
+void main() {
+  // calling the function we created above.
+  printName();
+}
