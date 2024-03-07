ce9674db7ccea2a9320f3875e21f78e60b3eff4c
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 13:21:09 +0300

    function with parameter and no return type

---
+++ Dart/functions_in_dart/parameter_function.dart
@@ -0,0 +1,9 @@
+// In this function, you do pass the parameter and expect no return type
+
+void main() {
+  printName("Richard Miruka");
+}
+
+void printName(String name){
+  print('Welcome home $name');
+}
