ad593585fcd08d3b9eb174b02097870059e94e8e
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 10:34:43 +0300

    switch statement in control flow in dart

---
+++ Dart/control_flow/switch_case_statement.dart
@@ -0,0 +1,19 @@
+void main() {
+  int i = 2;
+  switch (i) {
+    case 0:
+      print('Zero');
+      break;
+    case 1:
+      print('One');
+      break;
+    case 2:
+      print('Two');
+      break;
+    case 3:
+      print('Three');
+      break;
+    default:
+      print('The value is out of range');
+  }
+}
