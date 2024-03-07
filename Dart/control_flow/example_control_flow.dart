95af9b2f8f7077c61e63a951c5f843658125af85
RichardMiruka <richardonsare15@gmail.com>
2024-03-07 10:23:47 +0300

    simple control flows

---
+++ Dart/control_flow/example_control_flow.dart
@@ -0,0 +1,8 @@
+void main () {
+    var age = 18;
+    if (age >= 18) {
+        print('You are an adult');
+    } else {
+        print('You are not an adult');
+    }
+}
---
+++ Dart/control_flow/if_else.dart
@@ -0,0 +1,8 @@
+void main() {
+  var age = 10;
+  if (age >= 18) {
+    print('Mariam is a voter in Kenya');
+  } else {
+    print('Mariam is still young to vote');
+  }
+}
