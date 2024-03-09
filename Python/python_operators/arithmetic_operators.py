704bcab164223342178d0b79224c02120c2dc5ba
RichardMiruka <richardonsare15@gmail.com>
2024-03-09 18:47:26 +0300

    arithmetic operators in python

---
+++ Python/python_operators/arithmetic_operators.py
@@ -0,0 +1,15 @@
+# used to perform mathematical operations like addition, subtraction, multiplication.
+sub = 10 - 5; # returns 5
+add = 10 + 5; # returns 15
+mul = 10 * 5; # returns 50
+div = 10 / 2;   # returns 5.0
+
+# Modulus operator gives the remainder of a division operation.
+remainder = 7 % 4; # returns 3
+
+# Exponentiation operator raises the first operand to the power of the second operand.
+power = 2 ** 3;   # returns 8
+
+# Floor division operator (power) returns the quotient of a division operation, rounded down to the nearest integer.
+floor_division = 9 // 2; # returns 4
+
