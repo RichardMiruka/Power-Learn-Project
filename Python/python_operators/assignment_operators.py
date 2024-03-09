98374003afc71685c2a8824a8f36ebcbf71e6763
RichardMiruka <richardonsare15@gmail.com>
2024-03-09 19:04:36 +0300

    assignment operators in python

---
+++ Python/python_operators/assignment_operators.py
@@ -0,0 +1,11 @@
+# assignment operators are used to assign values to variables
+# x = 5; # assigns 5 to x and makes x equal to 5
+# y += 3; # adds 3 to y and makes y equal to 8. This is equivalent to y = y + 3
+# z -= 7; # subtracts 7 from z and makes z equal to 1. This is equivalent to z = z - 7
+
+x = 5; # assigns 5 to x and makes x equal to 5
+y = 0; # initializes variable y to 0
+y += 3; # adds 3 to y and makes y equal to 3. This is equivalent to y = y + 3
+
+m = 8 # assigns 8 to m and makes m equal to 8
+print("m is ", m, "\n") # m is 8
