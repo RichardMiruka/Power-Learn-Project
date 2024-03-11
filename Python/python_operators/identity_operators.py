039f62247027295330d24a06d19346cd9a7c3bc7
RichardMiruka <richardonsare15@gmail.com>
2024-03-11 12:32:25 +0300

    identity operators in python

---
+++ Python/python_operators/identity_operators.py
@@ -0,0 +1,10 @@
+# In Python, is and is not being used to check if two values are located on the same part of the memory.
+# Two equal variables do not imply that they are identical.
+
+# Identity operators are used to compare the memory locations of two objects. 
+# x is y checks whether x and y refer to the same object in memory. If so, it returns True; otherwise, it returns False.
+# They can be represented by "is" and "is not". The identity operator checks whether two variables point to the same memory location.
+# The "is" operator checks whether two variables point to the same memory location. If they do, it returns True; otherwise, it returns False.   The identity operator is used to compare the memory locations of two objects.
+# The "is not" operator checks whether two variables do not point to the same memory location. If they do not, it returns True; otherwise, it returns False.
+# The 'is' operator returns True if the variables on either side of the operator point to the same object and False otherwise.
+# x is y checks whether x and y point to the same object in memory. If so, it returns True; otherwise, it returns False.
