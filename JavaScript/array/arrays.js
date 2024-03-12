dd23cd06d29d81a4521696c89de99059c6a4ee59
RichardMiruka <richardonsare15@gmail.com>
2024-03-12 12:57:11 +0300

    introduction to arrays

---
+++ JavaScript/array/arrays.js
@@ -0,0 +1,4 @@
+/* eslint-disable project */
+// The Array object, as with arrays in other programming languages,
+// enables storing a collection of multiple items under a single variable name,
+// and has members for performing common array operations.
