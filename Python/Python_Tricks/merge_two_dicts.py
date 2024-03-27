3d7efd0c5523ac1765b24c0a86771ea152f9cb1d
RichardMiruka <richardonsare15@gmail.com>
2024-03-27 15:22:24 +0300

    merging two dictionaries in python

--- Python/Python_Tricks/merge_two_dicts.py
+++ Python/Python_Tricks/merge_two_dicts.py
@@ -8,6 +8,9 @@ z = {**x, **y}
 print (z)
 # Output: {'a': 1, 'b': 3, 'c': 4}
 
+# The code combines the key-value pairs from dictionaries x and y into a new dictionary z.
+# If there are duplicate keys, the values from y will overwrite the values from x.
+
 # In Python 2.x you could use this:
 # z = x.copy()
 # z.update(y)
