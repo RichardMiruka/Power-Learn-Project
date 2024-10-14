2c10d31fae9db2a891aeef948655a353ee77e5b6
RichardMiruka <richardonsare15@gmail.com>
2024-10-14 11:16:51 +0300

    generating an infinite fibonacci series by using generator

--- python/python_interview_questions/generator_infinite_fibonnacci_series.py
+++ python/python_interview_questions/generator_infinite_fibonnacci_series.py
@@ -1,10 +1,10 @@
 #Generate an infinite fibonacci series by using Generator
 
-def fibonacci():
+def fibonacci():              # The fibonacci function is a generator, 
     a, b = 0, 1
-    while True:
-        yield a
-        a, b = b, a + b
+    while True:               # The while True loop ensures the series continues indefinitely
+        yield a               # using the yield statement to return the next Fibonacci number each time next() is called
+        a, b = b, a + b       # The a, b = b, a + b updates the values for the next Fibonacci number in each iteration.
 
 f1 = fibonacci()
 print(next(f1))
@@ -16,3 +16,8 @@ print(next(f1))
 print(next(f1))
 print(next(f1))
 print(next(f1))
+
+# Example usage: Print the first 10 Fibonacci numbers
+# fib_gen = fibonacci()
+# for _ in range(10):
+    #print(next(fib_gen))
